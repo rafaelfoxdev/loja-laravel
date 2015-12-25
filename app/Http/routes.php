419d4d8f8219778a3d7edc21b1f5c41d928f3f1a
rafaelfoxdev <rafael.asouza@yahoo.com.br>
2015-12-25 20:02:18 -0200

    Fase 3

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,33 +1,52 @@
 <?php
 
 /*
-|--------------------------------------------------------------------------
-| Routes File
-|--------------------------------------------------------------------------
-|
-| Here is where you will register all of the routes in an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
-|
-*/
+  |--------------------------------------------------------------------------
+  | Routes File
+  |--------------------------------------------------------------------------
+  |
+  | Here is where you will register all of the routes in an application.
+  | It's a breeze. Simply tell Laravel the URIs it should respond to
+  | and give it the controller to call when that URI is requested.
+  |
+ */
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('admin/categories', 'AdminCategoriesController@index');
-Route::get('admin/products', 'AdminProductsController@index');
+Route::group(['prefix' => 'admin', 'where' => ['id' => '[0-9]+']], function() {
+
+    Route::group(['prefix' => 'categories'], function() {
+        Route::get('/', ['as' => 'categories', 'uses' => 'AdminCategoriesController@index']);
+        Route::post('/', ['as' => 'categories.store', 'uses' => 'AdminCategoriesController@store']);
+        Route::get('create', ['as' => 'categories.create', 'uses' => 'AdminCategoriesController@create']);
+        Route::get('{id}/destroy', ['as' => 'categories.destroy', 'uses' => 'AdminCategoriesController@destroy']);
+        Route::get('{id}/edit', ['as' => 'categories.edit', 'uses' => 'AdminCategoriesController@edit']);
+        Route::put('{id}/update', ['as' => 'categories.update', 'uses' => 'AdminCategoriesController@update']);
+    });
+    
+    Route::group(['prefix' => 'products'], function() {
+        Route::get('/', ['as' => 'products', 'uses' => 'AdminProductsController@index']);
+        Route::post('/', ['as' => 'products.store', 'uses' => 'AdminProductsController@store']);
+        Route::get('create', ['as' => 'products.create', 'uses' => 'AdminProductsController@create']);
+        Route::get('{id}/destroy', ['as' => 'products.destroy', 'uses' => 'AdminProductsController@destroy']);
+        Route::get('{id}/edit', ['as' => 'products.edit', 'uses' => 'AdminProductsController@edit']);
+        Route::put('{id}/update', ['as' => 'products.update', 'uses' => 'AdminProductsController@update']);
+    });
+    
+});
 
 /*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| This route group applies the "web" middleware group to every route
-| it contains. The "web" middleware group is defined in your HTTP
-| kernel and includes session state, CSRF protection, and more.
-|
-*/
+  |--------------------------------------------------------------------------
+  | Application Routes
+  |--------------------------------------------------------------------------
+  |
+  | This route group applies the "web" middleware group to every route
+  | it contains. The "web" middleware group is defined in your HTTP
+  | kernel and includes session state, CSRF protection, and more.
+  |
+ */
 
 Route::group(['middleware' => ['web']], function () {
     //
